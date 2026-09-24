c66fdc0046bae7450f2e20b25f60586e9567b880
أمنية يمكن أن تتحقق <fares24315@gmail.com>
2026-09-24 03:14:44 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.PHARMACY_FIREBASE_CONFIG = {
-  apiKey: 'PASTE_YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  databaseURL: 'https://YOUR_PROJECT-default-rtdb.firebaseio.com',
-  projectId: 'YOUR_PROJECT',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: '123456789012',
-  appId: '1:123456789012:web:abcdef1234567890'
-};
